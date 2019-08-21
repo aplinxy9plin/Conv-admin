7742790fa28da62cd2716cc8a82f56805aaef028
AplinXy9plin <evilberry@icloud.com>
2019-08-21 14:12:16 +0700

    Updates

---
+++ precache-manifest.241c489cf11a5642fdc8531af7080655.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f59d870e22bb2ec3b6f",
+    "url": "/Conv-admin/static/js/runtime~main.a9ae08f6.js"
+  },
+  {
+    "revision": "094ddd49d729b956451d",
+    "url": "/Conv-admin/static/js/main.b9411b19.chunk.js"
+  },
+  {
+    "revision": "c9abbac2bc99aff336e4",
+    "url": "/Conv-admin/static/js/2.ada5d218.chunk.js"
+  },
+  {
+    "revision": "c9abbac2bc99aff336e4",
+    "url": "/Conv-admin/static/css/2.72e42be2.chunk.css"
+  },
+  {
+    "revision": "47003b720b4b29f6c72a3d34df7afecf",
+    "url": "/Conv-admin/index.html"
+  }
+];
